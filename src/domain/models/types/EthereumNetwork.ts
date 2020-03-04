4d39be81775cff61e76411881e6be792a6294137
xgmolinax <xgmolinax@gmail.com>
2020-03-03 19:14:08 -0600

    Adding base types

---
+++ src/domain/models/types/EthereumNetwork.ts
@@ -0,0 +1,7 @@
+export enum EthereumNetwork {
+    Mainnet = 'homestead',
+    Morden = 'morden',
+    Ropsten = 'ropsten',
+    Rinkeby = 'rinkeby',
+    Kovan = 'kovan'
+}
---
+++ src/domain/models/types/index.ts
@@ -0,0 +1,2 @@
+export * from './EthereumNetwork';
+export * from './TransactionType';
