5c5004cac77d72edf747346acd599f716b024a25
xgmolinax <xgmolinax@gmail.com>
2020-03-05 22:13:37 -0600

    Initial controller implementation

---
+++ src/adapters/controllers/Token.controller.ts
@@ -0,0 +1,167 @@
+import { Controller } from '@core';
+import { IConfig } from '@config';
+import {
+    GetSupportedTokens,
+    GetTokenAllowance,
+    GetTokenBalance,
+    GetTokenMetadata,
+    GetTokenRate
+} from '@useCases/token';
+import {
+    ISupportedTokensService,
+    ITokenAllowanceService,
+    ITokenBalanceService,
+    ITokenMetadataService,
+    ITokenRateService
+} from '@services';
+import {
+    SupportedTokensSerializer,
+    TokenAllowanceSerializer,
+    TokenBalanceSerializer,
+    TokenMetadataSerializer,
+    TokenRateSerializer
+} from '@serializers/token';
+
+export class TokenController extends Controller {
+    private getSupportedTokens: GetSupportedTokens;
+    private getTokenAllowance: GetTokenAllowance;
+    private getTokenBalance: GetTokenBalance;
+    private getTokenMetadata: GetTokenMetadata;
+    private getTokenRate: GetTokenRate;
+
+    constructor(
+        supportedTokensImpl: ISupportedTokensService,
+        tokenAllowanceImpl: ITokenAllowanceService,
+        tokenBalanceImpl: ITokenBalanceService,
+        tokenMetadataImpl: ITokenMetadataService,
+        tokenRateImpl: ITokenRateService,
+        private config: IConfig
+    ) {
+        super();
+        this.getSupportedTokens = new GetSupportedTokens(supportedTokensImpl);
+        this.getTokenAllowance = new GetTokenAllowance(tokenAllowanceImpl);
+        this.getTokenBalance = new GetTokenBalance(tokenBalanceImpl);
+        this.getTokenMetadata = new GetTokenMetadata(tokenMetadataImpl);
+        this.getTokenRate = new GetTokenRate(tokenRateImpl);
+    }
+
+    public async supportedTokens({
+        network
+    }: {
+        network: string;
+    }): Promise<
+        Array<{
+            name: string;
+            symbol: string;
+            address: string;
+            decimals: number;
+            imageSource: string;
+        }>
+    > {
+        try {
+            const supportedTokens = await this.getSupportedTokens.execute(
+                SupportedTokensSerializer.toDomain({ network })
+            );
+            return this.success(
+                SupportedTokensSerializer.toDTO(supportedTokens)
+            );
+        } catch (error) {
+            this.fail(error);
+            throw error;
+        }
+    }
+
+    public async tokenAllowance({
+        walletAddress,
+        tokenAddress,
+        network
+    }: {
+        walletAddress: string;
+        tokenAddress: string;
+        network: string;
+    }): Promise<string> {
+        try {
+            const allowance = await this.getTokenAllowance.execute(
+                TokenAllowanceSerializer.toDomain({
+                    walletAddress,
+                    tokenAddress,
+                    contractAddress: this.config.stablepayContractAddress,
+                    network
+                })
+            );
+            return this.success(TokenAllowanceSerializer.toDTO(allowance));
+        } catch (error) {
+            this.fail(error);
+            throw error;
+        }
+    }
+
+    public async tokenBalance({
+        walletAddress,
+        tokenAddress,
+        network
+    }: {
+        walletAddress: string;
+        tokenAddress: string;
+        network: string;
+    }): Promise<string> {
+        try {
+            const balance = await this.getTokenBalance.execute(
+                TokenBalanceSerializer.toDomain({
+                    walletAddress,
+                    tokenAddress,
+                    network
+                })
+            );
+            return this.success(TokenBalanceSerializer.toDTO(balance));
+        } catch (error) {
+            this.fail(error);
+            throw error;
+        }
+    }
+
+    public async tokenMetadata({
+        symbol,
+        network
+    }: {
+        symbol: string;
+        network: string;
+    }): Promise<{
+        name: string;
+        symbol: string;
+        address: string;
+        decimals: number;
+        imageSource: string;
+    }> {
+        try {
+            const token = await this.getTokenMetadata.execute(
+                TokenMetadataSerializer.toDomain({ symbol, network })
+            );
+            return this.success(TokenMetadataSerializer.toDTO(token));
+        } catch (error) {
+            this.fail(error);
+            throw error;
+        }
+    }
+
+    public async tokenRate({
+        symbol,
+        network
+    }: {
+        symbol: string;
+        network: string;
+    }): Promise<{
+        expected: string;
+        slippage: string;
+    }> {
+        try {
+            const rate = await this.getTokenRate.execute(
+                TokenRateSerializer.toDomain({ symbol, network })
+            );
+            return this.success(TokenRateSerializer.toDTO(rate));
+        } catch (error) {
+            this.fail(error);
+            throw error;
+        }
+    }
+}
