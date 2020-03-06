c3305beb07d9e2d8bb3367894815fed8fa26f458
xgmolinax <xgmolinax@gmail.com>
2020-03-05 22:11:40 -0600

    Adding contract address to allowance

--- src/app/useCases/token/GetTokenAllowance.ts
+++ src/app/useCases/token/GetTokenAllowance.ts
@@ -6,6 +6,7 @@ import { EthereumNetwork } from '@types';
 interface IGetTokenAllowanceProps {
     walletAddress: EthereumAddress;
     tokenAddress: EthereumAddress;
+    contractAddress: EthereumAddress;
     network: EthereumNetwork;
 }
 
