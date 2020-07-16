6777f84797f54bd602a39bf87040b31f20aaaa5d
Guillermo Daniel Salazar <guillesalazar@gmail.com>
2020-07-15 22:18:04 -0300

    New version

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { getAPI } from '@api';
 import { ServicesContainer } from '@containers';
 import { IConfig } from '@config';
 
-export * from '@domain/models/types'
-
 export default (config: IConfig) => {
     const services = new ServicesContainer();
     return getAPI(config, services);
